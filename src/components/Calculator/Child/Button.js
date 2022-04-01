db213d59e98d974a71a29f7e59b5153344d4d6f2
Mahmoud Mostafa <Mahmoud@mightydigits.com>
2022-04-01 11:01:33 +0300

    finish the calculator

--- src/components/Calculator/Child/Button.js
+++ src/components/Calculator/Child/Button.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./button.scss";
 
-function Button({title, value}) {
+function Button({ title, value, input, setInput, output, setOutput }) {
+  const displayNumber = (e) => {
+    e.preventDefault();
+    let number = (input += e.target.value);
+    if (!number.includes('value')) {
+      setInput(number.replace(/^0/, ''))
+    }
+  };
+
+  const clear = () => {
+    setInput('0');
+    setOutput('0');
+  };
+  const deleteLastDigit = () => {
+    setInput(input.slice(0,-1)) // or you can use input.length-1
+  };
+
+  const percentage = () => {
+    setInput(input/100)
+  };
+
+  const calculate = () => {
+    try {
+      // let calculation = eval(input)
+      let calculation = Function('return ' + input)
+      setOutput(calculation)
+    } catch (error) {
+      setOutput(input.slice(0,-1))
+    }
+  };
+
   return (
-    <button>{title}</button>
-  )
+    <button
+      value={value}
+      onClick={
+        value === "ac"
+          ? clear
+          : displayNumber && value === "delete"
+          ? deleteLastDigit
+          : displayNumber && value === "%"
+          ? percentage
+          : displayNumber && value === "="
+          ? calculate
+          : displayNumber
+      }
+    >
+      {title}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
--- src/components/Calculator/Parent/Calculator.js
+++ src/components/Calculator/Parent/Calculator.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Child/Button";
 import "./calculator.scss";
-import items from '../calculatorData';
+import items from "../calculatorData";
 
 function Calculator() {
+
+  const [input, setInput] =useState("");
+  const [output, setOutput] = useState("");
+
   return (
     <div className="container">
       <div className="screen">
-        <div className="input">125</div>
+        <div className="input">{input}</div>
         <div className="output">
-          <span>=</span>258
+          <span>=</span>{output}
         </div>
       </div>
       <div className="keypad">
         {items.map((item) => (
-          <Button title={item.title} value={item.value}/>
-  ))}
-        
+          <Button title={item.title} value={item.value} input={input} setInput={setInput} output={output} setOutput={setOutput} />
+        ))}
       </div>
     </div>
   );
